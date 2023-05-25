672084d336b7e2eb4a949d0c471786df9d20c328
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-05-25 20:06:26 +0300

    Debug Script
    minor unoptimized changes to the script in order to debug it, on the surface looks like it works, need to check that further after the scene will be complete

--- Assets/Scripts/CounterTop/IngredientSpawnArea.cs
+++ Assets/Scripts/CounterTop/IngredientSpawnArea.cs
@@ -8,7 +8,8 @@ public class IngredientSpawnArea : MonoBehaviour
     // Public
     public IngredientType IngredientAreaType { get; private set; }
 
-
+    // debug check
+    public GameObject PrefabExample;
 
     // Serialized
     [SerializeField] Transform[] spawnPointsTransforms;
@@ -18,6 +19,8 @@ public class IngredientSpawnArea : MonoBehaviour
     // Private
     List<Ingredient> currentPickedIngredients;
 
+    List<GameObject> spawnedIngredents;
+
     float maximumNumberOfIngredients;
 
     int index = 0;
@@ -26,6 +29,10 @@ public class IngredientSpawnArea : MonoBehaviour
 
     private void Awake()
     {
+        IngredientAreaType = IngredientType.Alcohol;
+        currentPickedIngredients = new List<Ingredient>();
+        spawnedIngredents = new List<GameObject>();
+
         // Checks if the number of ingredients allowed to spawn is at minimun
         if (numberOfIngredientsCanSpawn > 3) numberOfIngredientsCanSpawn = 3;
 
@@ -35,39 +42,59 @@ public class IngredientSpawnArea : MonoBehaviour
         // Checks if there is an overlap between the maximun number of ingredients and the number of them that can spawn and cap that
         if (numberOfIngredientsCanSpawn > maximumNumberOfIngredients) numberOfIngredientsCanSpawn = maximumNumberOfIngredients;
 
-
-
     }
 
     // Should be called with the event
     /// <summary>
-    /// This method fires off when the player presses on one of the ingredients
+    /// This method fires off when the player presses on one of the ingredients (Needs to be optimized!)
     /// </summary>
     /// <param name="pressedIngredient"></param>
-    void IngredientPressedOverseer(Ingredient pressedIngredient)
+    void IngredientPressed(Ingredient pressedIngredient)
     {
         // Validates the pressed ingredient type equlas to the area type
         if (pressedIngredient.Type == IngredientAreaType)
         {
             bool ingredientExsitsInTheList = false;
-            // Checks if the ingredient is allready exists in the list of represented ingredients            
-            foreach(var ingredient in currentPickedIngredients)
+            // Checks if the ingredient is allready exists in the list of represented ingredients
+            // ! * ! needs more tunning and i know it, super unpotimized and will be changed ! * !
+            if (currentPickedIngredients.Count > 0)
             {
-                if (pressedIngredient == ingredient)
+                foreach(var ingredient in currentPickedIngredients)
                 {
-                    ingredientExsitsInTheList = true;
-                    currentPickedIngredients.Remove(ingredient);
-                    index --;
+                    if (pressedIngredient == ingredient)
+                    {
+                        foreach (var spawned in spawnedIngredents)
+                        {
+                            if (spawned.GetComponent<Ingredient>().Name == pressedIngredient.Name)
+                            {
+                                Destroy(spawned);
+                            }
+                        }
+                        ingredientExsitsInTheList = true;
+                        currentPickedIngredients.Remove(ingredient);
+                        index --;
+                        break;
+                    }
                 }
             }
 
             if (!ingredientExsitsInTheList && !IndexOverflow())
             {
                 currentPickedIngredients.Add(pressedIngredient);
+                spawnedIngredents.Add(Instantiate(PrefabExample, spawnPointsTransforms[index]));
                 index++;
             }
         }
 
+    }
+
+    // dedbug pourpse only !s
+    [ContextMenu("VodkaExample")]
+    public void VodkaExample()
+    {
+        Ingredient ingre = PrefabExample.GetComponent<Ingredient>();
+
+        IngredientPressed(ingre);
     }
 
     bool IndexOverflow()
