ce93b2594ce8dca6ca6810642513a85fcd8d2cd6
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-06-17 11:59:02 +0300

    Minigame base
    created the base for the shaker minigame

---
+++ Assets/Scripts/ShakerMinigame/Shaker.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Shaker : MonoBehaviour , IPointerDownHandler
+{
+    [SerializeField] Rigidbody2D rb2;
+    [SerializeField] float maxX;
+    [SerializeField] float minHeight;
+    [SerializeField] float maxHeight;
+
+    int shakerClickCount = 0;
+
+    float rndX;
+    float rndY;
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        ShakerClicked();
+    }
+
+    private void Awake()
+    {
+        rb2.simulated = false;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            rb2.simulated = true;
+        }
+    }
+
+    void ShakerClicked()
+    {
+        shakerClickCount++;
+        rndX = Random.Range(-maxX, maxX);
+        rndY = Random.Range(minHeight, maxHeight);
+
+        rb2.velocity = new Vector2(rndX, rndY);
+    }
+
+    
+}
