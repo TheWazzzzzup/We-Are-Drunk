7f0a8a2542f2150003e2f0dc7ee20174407d03eb
EladReuven <102731503+EladReuven@users.noreply.github.com>
2023-06-21 19:46:31 +0300

    inventory bar transition finished

---
+++ Assets/InventoryTransitionTrigger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class InventoryTransitionTrigger : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField] CameraController cameraController;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        switch(cameraController.currentState)
+        {
+            case MainSceneCameraState.Bar:
+                cameraController.MoveToInventory();
+                break;
+            case MainSceneCameraState.Transitioning: break;
+            case MainSceneCameraState.Inventory:
+                cameraController.MoveToBar();
+                break;
+        }
+    }
+}
