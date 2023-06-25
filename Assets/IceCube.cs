1e93eef4904c6708cb246f6542d6267425d59e12
EladReuven <102731503+EladReuven@users.noreply.github.com>
2023-06-25 19:03:01 +0300

    almost completed ice minigame, no points yet

--- Assets/IceCube.cs
+++ Assets/IceCube.cs
@@ -7,18 +7,55 @@ using UnityEngine.Tilemaps;
 public class IceCube : MonoBehaviour
 {
     [SerializeField] Rigidbody2D rb;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] BoxCollider2D boxCollider;
+
+
+    IceCubeState currentState;
 
     public Rigidbody2D Rb { get => rb;}
+    public IceCubeState CurrentState { get => currentState;}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (currentState != IceCubeState.Dropping)
+            return;
+
+        if(collision.gameObject.tag == "IceCube")
+        {
+
+        }
+    }
+
+    public void Initialize(Transform otherSide, float time, float scale)
+    {
+        currentState = IceCubeState.Swaying;
+        transform.localScale *= scale;
+        Sway(otherSide, time);
+    }
 
     public void Sway(Transform otherSide, float time)
     {
-        transform.DOMoveX(otherSide.position.x, time).SetEase(Ease.InOutExpo).SetLoops(-1);
+        transform.DOMoveX(otherSide.position.x, time).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
     }
 
     [ContextMenu("drop")]
     public void Drop()
     {
         transform.DOKill();
+        currentState = IceCubeState.Dropping;
         rb.simulated = true;
+    }
+
+    public void SetIceCubeStable()
+    {
+        currentState = IceCubeState.Stable;
+    }
+
+    public enum IceCubeState
+    {
+        Swaying,
+        Dropping,
+        Stable
     }
 }
--- Assets/Scripts/Minigames/IceMinigame/IceTowerGameManager.cs
+++ Assets/Scripts/Minigames/IceMinigame/IceTowerGameManager.cs
@@ -1,31 +1,131 @@
+using DG.Tweening;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class IceTowerGameManager : MonoBehaviour
 {
+    [SerializeField] Camera mainCamera;
+
+    [Header("Game parameters")]
+    [SerializeField] int targetScore = 10;
+    int currentScore = 0;
 
     [Header("Ice Cube Parameters")]
     [SerializeField] IceCube[] iceCubePrefabs;
     [SerializeField] IceCube currentIceCube;
-    [SerializeField] Transform targetSpawnLocation;
     [SerializeField] float time = 3f;
+    [SerializeField] float scale = 1f;
 
-    Stack<IceCube> placedIceCubes;
 
     [Header("SpawnLocations")]
-    [SerializeField] Transform parentSpawnLocation;
     [SerializeField] Transform leftSpawnLocation;
     [SerializeField] Transform rightSpawnLocation;
+    [SerializeField] Transform targetSpawnLocation;
+
+    Stack<IceCube> placedIceCubes = new();
 
     bool rightside = true;
+    float nextCubeTimer = 3f;
+    float timer = 4f;
+    bool isGameOver = false;
 
+    public IceCube CurrentIceCube { get => currentIceCube;}
+
+    private void OnMouseDown()
+    {
+        if(currentIceCube == null) 
+            return;
+
+        if(currentIceCube.CurrentState == IceCube.IceCubeState.Swaying)
+        {
+            currentIceCube.Drop();
+        }
+    }
+
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void Update()
+    {
+        if(isGameOver)
+        {
+            return;
+        }
+
+        if(currentScore >= targetScore)
+        {
+            print("You win");
+            GameOver();
+            return;
+        }
+
+        //check if null
+        if (currentIceCube == null)
+            return;
+
+        //check if dropping
+        if (currentIceCube.CurrentState != IceCube.IceCubeState.Dropping)
+            return;
+
+        if (timer <= 0)
+        {
+            //add ice cube to stack, get next ice cube
+            placedIceCubes.Push(currentIceCube);
+            PrepareForNextIceCube();
+        }
+        else
+        {
+            timer -= Time.deltaTime;
+        }
+
+    }
+
+    public void StartGame()
+    {
+        PrepareForNextIceCube();
+    }
 
     [ContextMenu("get icecube")]
-    void GetIceCube()
+    void GetNextIceCube()
     {
         currentIceCube = SpawnIceCube();
-        currentIceCube.Sway(targetSpawnLocation, time);
+        scale = RandomScale();
+        currentIceCube.Initialize(targetSpawnLocation, time, scale);
+        timer = nextCubeTimer;
+    }
+
+    void PrepareForNextIceCube()
+    {
+        if (currentIceCube == null)
+        {
+            GetNextIceCube();
+        }
+        else
+        {
+            currentIceCube.SetIceCubeStable();
+            time *= 0.9f;
+            GainPoints();
+            MoveGameUp();
+        }
+    }
+
+    void GainPoints()
+    {
+        currentScore++;
+    }
+
+    void MoveGameUp()
+    {
+        Vector3 upPosition = new Vector3(transform.position.x, transform.position.y + (2 * scale), transform.position.z);
+        mainCamera.transform.DOMoveY(mainCamera.transform.position.y + 2 * scale, 0.3f).SetEase(Ease.OutSine).OnComplete(() => {
+            transform.position = upPosition;
+            GetNextIceCube();
+            });
     }
 
     public IceCube SpawnIceCube()
@@ -46,6 +146,17 @@ public class IceTowerGameManager : MonoBehaviour
         return newIceCube;
     }
 
+    public void GameOver()
+    {
+        if (isGameOver)
+            return;
+
+        isGameOver = true;
+        print("GameOver");
+        //game over logic - show score, send score, start timer to send back to main scene
+    }
+
+    #region GetRandom
     bool RandomSpawnSide()
     {
         if (Random.Range(0, 2) == 1)
@@ -61,4 +172,10 @@ public class IceTowerGameManager : MonoBehaviour
 
         return rightside;
     }
+
+    float RandomScale()
+    {
+        return Random.Range(0.8f, 1.2f);
+    }
+    #endregion
 }
