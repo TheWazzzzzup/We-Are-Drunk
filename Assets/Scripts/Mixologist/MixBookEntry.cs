55bea0fc24f45d1d28ff878411af9b3b8131919d
GDIVX <yarin107@gmail.com>
2023-07-21 17:28:52 +0300

    Created UI for the book

--- Assets/Scripts/Mixologist/MixBookEntry.cs
+++ Assets/Scripts/Mixologist/MixBookEntry.cs
@@ -1,3 +1,4 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -10,13 +11,12 @@ public class MixBookEntry : MonoBehaviour
     [SerializeField] Image recipeImage;
     [SerializeField] TMP_Text recipeName;
     [SerializeField] TMP_Text recipeIngredients;
-    [SerializeField] TMP_Text recipeDifficlty;
 
+    [Button("AssigenToUIElement")]
     public void AssigenToUIElement(MixSO mixSO)
     {
         recipeImage.sprite = mixSO.drinkSprite;
         recipeName.text = mixSO.name;
-        recipeDifficlty.text = mixSO.difficutly.ToString();
         string ingredientsText = "";
         foreach (var ingredient in mixSO.recipe.Ingredients)
         {
